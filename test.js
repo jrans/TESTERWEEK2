ee22b4d71b64a342a8094c61b2f7749aec0b9fa0
Jack.Rans <jwrans@gmail.com>
2015-05-18 13:52:36 +0100

    Update test files with wireframes

--- test.js
+++ test.js
@@ -2,7 +2,18 @@ test("this bit should describe, to a human, what the test is for", function() {
     equal( 1, 1);
 });
 
+
+
+
+
+
+
 test("check that the header exist'", function() {
-    var title= document.getElementById('heading').innerHTML;
-    equal(title, "This is a header");
+    var iframe = document.getElementById('if-index');
+
+    var target = iframe.contentDocument || iframe.contentWindow.document;
+
+    var initial = target.getElementById('heading');
+
+    equal(initial, 'Hello World!', 'it works');
 });
