265b0385b0c2c161db62e51b4a61e36cdddcbbbc
Anni Vaananen <anni.vaananen@gmail.com>
2015-05-18 16:49:07 +0100

    our changes

--- test.js
+++ test.js
@@ -6,36 +6,44 @@ test("this bit should describe, to a human, what the test is for", function() {
 
 
 
+test("If there is a header", function(){
+var iframe = document.getElementById('if-index');
+var target = iframe.contentDocument || iframe.contentWindow.document;
+var testable = target.getElementById('heading').innerHTML;
+
+notEqual(testable,'',"Woohoo, the header exists and has some text in it")
+
+})
 
 
-test("check that the header exist'", function() {
-    var iframe = document.getElementById('if-index');
+test("Is there an image on the page", function(){
+var iframe = document.getElementById('if-index');
+var target = iframe.contentDocument || iframe.contentWindow.document;	
+var testimage = target.getElementsByTagName("img")[0].alt;
 
-    var target = iframe.contentDocument || iframe.contentWindow.document;
+notEqual(testimage, "", "yeah");
 
-    var initial = target.getElementById('heading');
+})
 
-    equal(initial, 'Hello World!', 'it works');
-});
+test("We want to see a css link", function(){
+var iframe = document.getElementById('if-index');
+var target = iframe.contentDocument || iframe.contentWindow.document;	
+var linkki = target.getElementsByTagName('link')[0].href;
 
-SC.initialize({
-  client_id: 'a3f9118b2e28c73d8d2e68a609815e7d'
-});
 
-$(document).ready(function() {
-    SC.stream('/tracks/293', function(sound) {
-        $( "#start" ).click(function() {
-            sound.start();
-        }),
-        $( "#stop" ).click(function() {
-            sound.stop();
-        });
-});
-});
+equal(linkki,"main.css", "Fuck yessss! We are ze besttt testers on the face of earth");
 
-$(document).ready(function() {
-    SC.get('/tracks/42242000', function(track) {
-      SC.oEmbed(track.permalink_url, document.getElementById('player'));
-    });
-}
-)
+})
+
+test("We want to test if the font family of the header is sans serif", function(){
+var iframe = document.getElementById('if-index');
+var target = iframe.contentDocument || iframe.contentWindow.document;	
+var  elementti = target.getElementById('heading')
+var fontstyle =window.getComputedStyle(elementti,null).getPropertyValue('font-family');
+
+equal(fontstyle,"sans-serif","Onneksi olkoon you are the mosts awesomest testing team!");
+
+})
+
+
+
